3eb46d264d5a9a686b4bb20dbd3266f6ba6a24e7
wclifton1 <wclifton1@gmail.com>
2014-01-05 00:03:15 -0600

    good through the story part

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 function fadedEls(el, shift) {
     el.css('opacity', 0);
-
+    
     switch (shift) {
         case undefined:
             shift = 0;
@@ -12,7 +12,7 @@ function fadedEls(el, shift) {
             shift = el.eq(0).outerHeight() / 2;
             break;
     }
-
+    
     $(window).resize(function() {
         if (!el.hasClass('ani-processed')) {
             el.eq(0).data('scrollPos', el.eq(0).offset().top - $(window).height() + shift);
@@ -39,36 +39,58 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
 
 (function($) {
     $(function() {
-       
+        
         // Focus state for append/prepend inputs
         $('.input-prepend, .input-append').on('focus', 'input', function() {
             $(this).closest('.control-group, form').addClass('focus');
         }).on('blur', 'input', function() {
             $(this).closest('.control-group, form').removeClass('focus');
         });
- 
-       
-
+        
+        
+        
         // features ani
         fadedEls($('.features').parent().find('h3'), 'h');
         $('.features > *').each(function() {
             fadedEls($(this), 150);
-        });     
+        });    
         
-
+        //will
+        
+        $("#nav ul li a[href^='#']").on('click', function(e) {
+            
+            // prevent default anchor click behavior
+            e.preventDefault();
+            
+            // store hash
+            var hash = this.hash;
+            
+            // animate
+            $('html, body').animate({
+                scrollTop: $(this.hash).offset().top
+            }, 600, function(){
+                
+                // when done, add hash to url
+                // (default click behaviour)
+                window.location.hash = hash;
+            });
+            
+        });
+        
+        
         // responsive
         $(window).resize(function() {
             // input-append auto width
             $('footer .input-append input[type="text"]').each(function() {
                 var controlGroup = $(this).closest('.control-group');
-
+                
                 if ($(window).width() > 480) {
                     $(this).css('width', '');
                 } else {
                     $(this).css('width', controlGroup.outerWidth() - controlGroup.find('.input-append .btn').outerWidth());
                 }
             });
-
+            
             // social-btns
             if ($(window).width() > 480) {
                 $('footer .social-btns.mobile-processed').removeClass('mobile-processed').appendTo('footer > .container > .row > .span3:last');
@@ -76,8 +98,8 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
                 $('footer .social-btns:not(.mobile-processed)').addClass('mobile-processed').insertBefore('footer nav');
             }
         });
-
+        
         $(window).resize().scroll();
-
+        
     });
 })(jQuery);

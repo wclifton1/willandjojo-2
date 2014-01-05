1537de7711a487c4ba4302ceafa7f10cb5c6cda7
wclifton1 <wclifton1@gmail.com>
2014-01-05 01:37:35 -0600

    restored parallax

--- js/script.js
+++ js/script.js
@@ -80,7 +80,7 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         
          // Parallax
         $('.content-23.first').parallax('50%', 0.3, true);
-        //$('.header-1-sub .background').parallax('50%', -0.3, true);
+        $('.header-1-sub .background').parallax('50%', -0.3, true);
 
         
         // responsive

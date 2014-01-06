43edb4ec80144a77f9827175dd7dc9e6d11bc6c6
wclifton1 <wclifton1@gmail.com>
2014-01-05 23:00:37 -0600

    all sorts of mess

--- js/script.js
+++ js/script.js
@@ -79,8 +79,10 @@ if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
         });
         
          // Parallax
-        $('.content-23.first').parallax('50%', 0.3, true);
-        $('.header-1-sub .background').parallax('50%', -0.3, true);
+        //$('.content-23.first').parallax('50%', 0.3, true);
+        //$('.content-23.second').parallax('50%', 0.3, true);
+
+        //$('.header-1-sub .background').parallax('50%', -0.3, true);
 
         
         // responsive
